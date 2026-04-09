d7eecc1a1d0c10ee434c4ca76ec8fdec2131b2df
Jurien Hamaker <whoami@jurien.dev>
2026-04-09 16:30:32 +0200

    feat: Add parseModalDataV2 for components v2

--- modal-utils.go
+++ modal-utils.go
@@ -1,6 +1,8 @@
 package discordgoplus
 
 import (
+	"fmt"
+
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -17,6 +19,53 @@ func ParseModalData(
 				data[txtInput.CustomID] = txtInput.Value
 			}
 		}
+	}
+
+	return data
+}
+
+func ParseModalDataV2(
+	modalData discordgo.ModalSubmitInteractionData,
+) map[string]interface{} {
+	data := make(map[string]interface{}, 0)
+	for _, row := range modalData.Components {
+		labelRow := row.(*discordgo.Label)
+		cmpnt := labelRow.Component
+
+		if cmpnt.Type() == 3 {
+			input := cmpnt.(*discordgo.SelectMenu)
+
+			if input.MaxValues == 1 || input.MaxValues == 0 {
+				data[input.CustomID] = input.Values[0]
+				continue
+			}
+
+			data[input.CustomID] = input.Values
+		}
+
+		if cmpnt.Type() == 4 {
+			input := cmpnt.(*discordgo.TextInput)
+			data[input.CustomID] = input.Value
+			continue
+		}
+
+		if cmpnt.Type() == 19 {
+			input := cmpnt.(*discordgo.FileUpload)
+
+			if input.MaxValues == 1 || input.MaxValues == 0 {
+				data[input.CustomID] = modalData.Resolved.Attachments[input.Values[0]]
+				continue
+			}
+
+			values := make([]*discordgo.MessageAttachment, 0)
+			for _, value := range input.Values {
+				values = append(values, modalData.Resolved.Attachments[value])
+			}
+
+			data[input.CustomID] = values
+		}
+
+		fmt.Println(cmpnt.Type())
 	}
 
 	return data
